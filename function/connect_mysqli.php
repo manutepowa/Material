0e5b0bf2ec614185acc6604d3a051f4636f0bad2
agarciaguillo <albertogarciaelx@gmail.com>
2015-12-16 13:14:57 +0100

    Prueba clone

--- function/connect_mysqli.php
+++ function/connect_mysqli.php
@@ -17,7 +17,7 @@
 	</div>
 	*/
 	function connect(){
-		require_once($_SERVER['DOCUMENT_ROOT'].'Material/config/config.php');
+		require_once($_SERVER['DOCUMENT_ROOT'].'/Material/config/config.php');
 		$mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 		return $mysqli;
 	}
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 	
 	// require_once('config/config.php');
-	require_once($_SERVER['DOCUMENT_ROOT'].'Material/function/connect_mysqli.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/Material/function/connect_mysqli.php');
 	$conexion = connect();
 	if ($conexion->connect_error) {
 		die("Fallo al conectar: ".$conexion->connect_error);
@@ -8,7 +8,7 @@
 	}
 	
 
-	
+
 
  ?>
 
