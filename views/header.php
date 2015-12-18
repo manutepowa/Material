51d09ff524488bfd79ab53ca0c38acc489652299
agarciaguillo <albertogarciaelx@gmail.com>
2015-12-18 13:46:57 +0100

    header y footer ok

--- views/header.php
+++ views/header.php
@@ -14,9 +14,9 @@
  <head>
  	<meta charset="UTF-8">
  	<title>Materiales OC</title>
- 	<link rel="shortcut icon" href="site_map/images/favicon.png">
- 	<link rel="stylesheet" href="site_map/Semantic-UI-CSS-master/semantic.min.css">
- 	<link rel="stylesheet" href="site_map/js/main.js">
+ 	<link rel="shortcut icon" href="../site_map/images/favicon.png">
+ 	<link rel="stylesheet" href="../site_map/Semantic-UI-CSS-master/semantic.min.css">
+ 	<link rel="stylesheet" href="../site_map/js/main.js">
  </head>
  <body>
  	
