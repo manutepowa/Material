21426387bc6eba033e3bda0c0855f04a24cf2303
agarciaguillo <albertogarciaelx@gmail.com>
2015-12-18 13:38:46 +0100

    pruebas alberto

---
+++ prueba.php
@@ -0,0 +1,35 @@
+<?php 
+	require_once($_SERVER['DOCUMENT_ROOT'].'/Material/views/header.php');
+ ?>
+
+<div class="ui bottom attached segment pushable">
+  <div class="ui visible inverted left vertical sidebar menu">
+    <a class="item">
+      <i class="home icon"></i>
+      Home
+    </a>
+    <a class="item">
+      <i class="block layout icon"></i>
+      Topics
+    </a>
+    <a class="item">
+      <i class="smile icon"></i>
+      Friends
+    </a>
+    <a class="item">
+      <i class="calendar icon"></i>
+      History
+    </a>
+  </div>
+  <div class="pusher">
+    <div class="ui basic segment">
+      <h3 class="ui header">Application Content</h3>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  </div>
+</div>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/Material/views/footer.php'); ?>
