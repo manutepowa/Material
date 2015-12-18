fb7bd792e1df89af501e78696b0910d73390904f
agarciaguillo <albertogarciaelx@gmail.com>
2015-12-18 13:43:01 +0100

    corregidos header y footer

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 	
-	<script src="../site_map/js/jquery-1.11.3.min.js"></script>
-	<script src="../site_map/Semantic-UI-CSS-master/semantic.min.js"></script>
-	<script src="../site_map/js/main.js"></script>
+	<script src="site_map/js/jquery-1.11.3.min.js"></script>
+	<script src="site_map/Semantic-UI-CSS-master/semantic.min.js"></script>
+	<script src="site_map/js/main.js"></script>
 	
 </body>
 </html>
