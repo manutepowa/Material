306da4be35a68378bd3d877dea2835a2d683d228
agarciaguillo <albertogarciaelx@gmail.com>
2015-12-21 14:01:21 +0100

    corregido liada prueba y prueba 2

---
+++ views/prueba2.php
@@ -0,0 +1,137 @@
+<?php 
+	require_once($_SERVER['DOCUMENT_ROOT'].'/Material/views/header.php');
+ ?>
+
+
+  <div class="cabecera">
+    <div class="ui container">
+      <!-- <div class="lmenu"> -->
+        <img src="../site_map/images/logo.png" alt="logo">
+        Materiales SC
+      <!-- </div> -->
+      <!-- <div class="rmenu"> -->
+        <a href="salir.php">Cerrar sesión</a>
+      <!-- </div> -->
+      </div>
+      </div>
+
+<div class="ui container">
+
+Filtrar por:
+  <div class="ui search">
+  <input class="prompt" type="text" placeholder="Common passwords...">
+  <div class="results"></div>
+</div>
+
+    <table class="ui teal table">
+      <thead>
+        <tr>
+          <th>MATERIAL</th>
+          <th>PERSONA</th>
+          <th>LUGAR</th>
+          <th>F. PRÉSTAMO</th>
+          <th>F. DEVOLUCIÓN</th>
+          <th>ACCIÓN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Camara Sony X10 Photo</td>
+          <td>Alberto García Guilló</td>
+          <td>Elche</td>
+          <td>18/12/2015</td>
+          <td>18/12/2015</td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Grabadora de mano Z6</td>
+          <td>Ángel Fuentes Mirete</td>
+          <td>Alicante</td>
+          <td>18/12/2015</td>
+          <td>19/12/2015</td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Camara Sony X10 Photo</td>
+          <td>Alberto García Guilló</td>
+          <td>Elche</td>
+          <td>19/12/2015</td>
+          <td></td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Grabadora de mano Z6</td>
+          <td>Ángel Fuentes Mirete</td>
+          <td>Alicante</td>
+          <td>20/12/2015</td>
+          <td></td>
+          <td>editar/borrar</td>
+        </tr><tr>
+          <td>Camara Sony X10 Photo</td>
+          <td>Alberto García Guilló</td>
+          <td>Elche</td>
+          <td>18/12/2015</td>
+          <td>18/12/2015</td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Grabadora de mano Z6</td>
+          <td>Ángel Fuentes Mirete</td>
+          <td>Alicante</td>
+          <td>18/12/2015</td>
+          <td>19/12/2015</td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Camara Sony X10 Photo</td>
+          <td>Alberto García Guilló</td>
+          <td>Elche</td>
+          <td>19/12/2015</td>
+          <td></td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Grabadora de mano Z6</td>
+          <td>Ángel Fuentes Mirete</td>
+          <td>Alicante</td>
+          <td>20/12/2015</td>
+          <td></td>
+          <td>editar/borrar</td>
+        </tr><tr>
+          <td>Camara Sony X10 Photo</td>
+          <td>Alberto García Guilló</td>
+          <td>Elche</td>
+          <td>18/12/2015</td>
+          <td>18/12/2015</td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Grabadora de mano Z6</td>
+          <td>Ángel Fuentes Mirete</td>
+          <td>Alicante</td>
+          <td>18/12/2015</td>
+          <td>19/12/2015</td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Camara Sony X10 Photo</td>
+          <td>Alberto García Guilló</td>
+          <td>Elche</td>
+          <td>19/12/2015</td>
+          <td></td>
+          <td>editar/borrar</td>
+        </tr>
+        <tr>
+          <td>Grabadora de mano Z6</td>
+          <td>Ángel Fuentes Mirete</td>
+          <td>Alicante</td>
+          <td>20/12/2015</td>
+          <td></td>
+          <td>editar/borrar</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/Material/views/footer.php'); ?>
