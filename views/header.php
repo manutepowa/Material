08ffc37f56122f414fda125c8e8a03758930666b
Manuel <manutepowa@gmail.com>
2015-12-22 11:35:49 +0100

    ..

--- views/header.php
+++ views/header.php
@@ -17,10 +17,8 @@
  	<link rel="shortcut icon" href="../site_map/images/favicon.png">
  	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
  	<link rel="stylesheet" href="../site_map/Semantic-UI-CSS-master/semantic.min.css">
-<<<<<<< HEAD
  	<link rel="stylesheet" href="../site_map/css/listar_style.css">
-=======
  	<link rel="stylesheet" href="../site_map/css/style.css">
->>>>>>> a8a413f686a14fbe0333762f92192ccbb1c3e6fe
+
  </head>
  <body>
