b3c300496bbf0c1a3f370229cfbc0f525a529f3b
Manuel <manutepowa@gmail.com>
2015-12-23 10:07:03 +0100

    filtro de datos hecho

--- site_map/js/filter.js
+++ site_map/js/filter.js
@@ -5,12 +5,32 @@ $(document).ready(function(){
         /* Useful DOM data and selectors */
         var $input = $(this),
         inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filtros'),
+        column = $panel.find($input.parents('div')).index() - 1;
+
+
+        /*Hasta aqui funciona*/
+        $container = $panel.parents('.container'),
+        $table = $container.find('.table'),
+        $rows = $table.find('tbody tr');
+        // $('body').html($table);
+
+		var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        console.log($filteredRows);
+
+
+		$table.find('tbody .no-result').remove();
         
+        $rows.show();
+        $filteredRows.hide();
 
-
-
-
-        console.log($inputContent);
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td>No hay resultados</td></tr>'));
+        }
+        
 
     });
 
--- views/listar.php
+++ views/listar.php
@@ -1,12 +1,7 @@
 <?php 
 	require_once('header.php');
  ?>
-<h1>Materiales</h1>
 
-<button id="op-modal" class="ui default right labeled icon button">
-	Añadir Préstamo
-	<i class="plus icon"></i>
-</button>
 
 <div class="ui small modal">
   <div class="header">
@@ -91,6 +86,77 @@
     Préstamo añadido con éxito!
   </div>
 </div>
+
+
+
+<div class="cabecera">
+    <div class="ui container">
+        <img src="../site_map/images/logo.png" alt="logo">
+        Materiales SC
+        <a href="../function/salir.php">Cerrar sesión</a>
+    </div>
+  </div>
+
+<div class="ui container">
+
+
+  <!-- <div class="ui search">
+    <button class="ui teal button"><i class="add icon"></i>Préstamo</button>
+    Filtrar por:
+    <input class="prompt" type="text" placeholder="Material...">
+    <input class="prompt" type="text" placeholder="Persona...">
+    <input class="prompt" type="text" placeholder="Lugar...">
+    <input class="prompt" type="text" placeholder="Fecha préstamo...">
+    <input class="prompt" type="text" placeholder="Fecha devolución...">
+    <div class="results"></div>
+  </div> -->
+  <div class="filtros">
+    <div class="nprestamo">
+      <button id="op-modal" class="ui teal button"><i class="add icon"></i>Préstamo</button>
+      <span>Filtrar por:</span>
+    </div>
+    <div class="filtro">
+      <input class="prompt" type="text" placeholder="Material...">
+      <!-- <div class="results"></div> -->
+    </div>
+    <div class="filtro">
+      <input class="prompt" type="text" placeholder="Persona...">
+      <!-- <div class="results"></div> -->
+    </div>
+    <div class="filtro">
+      <input class="prompt" type="text" placeholder="Lugar...">
+      <!-- <div class="results"></div> -->
+    </div>
+    <div class="filtro">
+      <input class="prompt" type="text" placeholder="Fecha préstamo...">
+      <!-- <div class="results"></div> -->
+    </div>
+    <div class="filtro">
+      <input class="prompt" type="text" placeholder="Fecha devolución...">
+      <!-- <div class="results"></div> -->
+    </div>
+  </div>
+  <!-- <div class="results"></div> -->
+  <!-- </div> -->
+
+    <table id="prestamos" class="ui teal table">
+      <thead>
+        <tr>
+          <th>MATERIAL</th>
+          <th>PERSONA</th>
+          <th>LUGAR</th>
+          <th>F. PRÉSTAMO</th>
+          <th>F. DEVOLUCIÓN</th>
+          <th>ACCIÓN</th>
+        </tr>
+      </thead>
+      <tbody>
+      <!-- http://bootsnipp.com/snippets/featured/panel-table-with-filters-per-column -->
+      </tbody>
+    </table>
+
+  </div>
+
 
 
 <?php require_once('footer.php'); ?>
