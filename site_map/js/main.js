e6e995b83ef157b85145fc55263ce52a3915d342
adefuentes <adefuentes93@gmail.com>
2015-12-23 13:25:51 +0100

    arreglo de interfaz en tabla

--- site_map/js/main.js
+++ site_map/js/main.js
@@ -109,6 +109,7 @@ $(document).ready(function(){
 	$("#prestamos tbody").html("");
 
 	var cnt = 0;
+	var error_field = 0;
 
 	$.getJSON(urlprestamo, function(prest){
 		for (var j = 0; j < urlprestamo.length; j++) {
@@ -116,15 +117,25 @@ $(document).ready(function(){
 
 				if(cnt != 12){
 					var fDev;
-					if(p[j][5]){fDev=p[j][5];}else{fDev="no devuelto";}
+					if(p[j][5]){fDev=p[j][5];}else{fDev="no devuelto"; error_field=1;}
 					
 					var newRow = "<tr value='"+p[j][0]+"'>"
 									+"<td>"+p[j][1]+"</td>"
 									+"<td>"+p[j][2]+"</td>"
 									+"<td>"+p[j][3]+"</td>"
-									+"<td>"+p[j][4]+"</td>"
-									+"<td>"+fDev+"</td>"
+									+"<td>"+p[j][4]+"</td>";
+
+					if(error_field == 1){
+						newRow += +"<td class='negative'>"+fDev+"</td>"
 								+"</tr>";
+
+						error_field = 0;
+					}
+					else{
+						newRow += +"<td>"+fDev+"</td>"
+								+"</tr>";
+					}
+									
 					$(newRow).appendTo("#prestamos tbody");
 
 					cnt++;
