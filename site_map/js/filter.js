7e837898b6647a6cc658a5d14564907c58beb257
adefuentes <adefuentes93@gmail.com>
2016-01-08 13:10:54 +0100

    filter casi arreglado

--- site_map/js/filter.js
+++ site_map/js/filter.js
@@ -1,35 +1,57 @@
+var first = 1;
+
 $(document).ready(function(){
 	$('.filtros .filtro input').keyup(function(e){
 		var code = e.keyCode || e.which;
         if (code == '9') return;
-        /* Useful DOM data and selectors */
+        
         var $input = $(this),
-        inputContent = $input.val().toLowerCase(),
-        $panel = $input.parents('.filtros'),
+        inputContent = $input.val().toLowerCase();
+        $panel = $input.parents('.filtros');
         column = $panel.find($input.parents('div')).index() - 1;
 
+        $container = $panel.parents('.container');
+        $table = $container.find('.table');
+        
 
-        /*Hasta aqui funciona*/
-        $container = $panel.parents('.container'),
-        $table = $container.find('.table'),
-        $rows = $table.find('tbody tr');
-        // $('body').html($table);
+        if(first == 1){
+            $allInfo = $table.find('tbody tr');
+        }
+        else{
+            
+        }
+
+        if (code  == '8'){
+            $rows = $allInfo;
+        }
+        else{
+            if(first == 1){
+                $rows = $allInfo;
+            }
+            else{
+                $rows = $partialRows;
+            }
+        }
 
 		var $filteredRows = $rows.filter(function(){
             var value = $(this).find('td').eq(column).text().toLowerCase();
             return value.indexOf(inputContent) === -1;
         });
-        console.log($filteredRows);
+        // console.log($filteredRows);
 
 
 		$table.find('tbody .no-result').remove();
         
-        $rows.show();
-        $filteredRows.hide();
+        // $rows.show();
+        $filteredRows.remove();
+
+        
 
         if ($filteredRows.length === $rows.length) {
             $table.find('tbody').prepend($('<tr class="no-result text-center"><td>No hay resultados</td></tr>'));
         }
+
+        first = 0;
         
 
     });
